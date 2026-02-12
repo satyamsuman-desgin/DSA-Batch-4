d352edabba90ed91698df5e292489023bf15fef6
Satyam kr Suman <jhalina3255@gmail.com>
2026-02-12 23:13:54 +0530

    Create 53.java
    Refactor maxSubArray method to handle negative sums.

--- 53.java
+++ 53.java
@@ -1,9 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
+        int currentSum = 0;
+        int maxSum = -99999;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum+=nums[i];
+            if(currentSum<0){
+                currentSum=0;
+            }
             maxSum = Math.max(maxSum, currentSum);
         }
         return maxSum;
