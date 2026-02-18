79bda7e7feaf83c03b336391be86f377904fd58b
Satyam kr Suman <jhalina3255@gmail.com>
2026-02-18 12:30:44 +0530

    Update 278.java
    Fix binary search logic to correctly find the first bad version.

--- 278.java
+++ 278.java
@@ -2,16 +2,18 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
-        while (left < right) {
+        int ans= n;
+        while (left <=right) {
             int mid = left + (right - left) / 2;
             if (isBadVersion(mid)) {
-                right = mid;
+                right = mid-1;
+                ans=mid;
             } else {
                 left = mid + 1;
             }
         }
         
-        return left;
+        return ans;
         
     }
 }
