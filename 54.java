b47aa53cb9029ec4916c5c5657a281b5dd2bed59
Satyam kr Suman <jhalina3255@gmail.com>
2026-03-05 12:27:00 +0530

    Implement spiral order traversal for matrix

---
+++ 54.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+        List<Integer> ans = new ArrayList<>();
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++)
+                ans.add(matrix[top][i]);
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                ans.add(matrix[i][right]);
+            right--;
+
+            for (int i = right; i >= left && top <= bottom; i--)
+                ans.add(matrix[bottom][i]);
+            bottom--;
+
+            for (int i = bottom; i >= top && left <= right; i--)
+                ans.add(matrix[i][left]);
+            left++;
+        }
+
+        return ans;
+    }
+}
